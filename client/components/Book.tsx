85ac14c5e39b44e0f312322810778d4e6bf8064a
Giovanni Ambriz <giovanni.ambriz@hotmail.com>
2024-08-14 13:55:39 +1200

    Create functions to show list of books and book info for book component

--- client/components/Book.tsx
+++ client/components/Book.tsx
@@ -1,7 +1,22 @@
-export default function Book() {
+interface Props {
+  title: string
+  author: string
+  description: string
+}
+
+export default function Book({ title, author, description }: Props) {
   return (
     <>
       <h2>Book details:</h2>
+      <div>
+        <h3>
+          <Link to={`/books/${id}`}>{title}</Link>
+        </h3>
+        <h4>{author}</h4>
+        <p>{description}</p>
+        <button>Review this book</button>
+      </div>
+
     </>
   )
 }
--- client/components/BooksList.tsx
+++ client/components/BooksList.tsx
@@ -1,7 +1,29 @@
+import { useQuery } from "@tanstack/react-query"
+import Book from "./Book"
+// import function from apiClient 
+
 export default function BooksList() {
+  const { data: books, isPending, isError } = useQuery({
+    queryKey: ['books'],
+    queryFn: fetchBooks() /* change accordinaly to apiClient function*/
+  })
+
+  if (isError) {
+    return <p>Error fetching list of books...</p>
+  }
+
+  if (isPending) {
+    return <p>Loading...</p>
+  }
+
   return (
     <>
       <h2>List of books:</h2>
+      {books.map((book) => {
+        return <Book key={id} id={book.id} title={book.title} author={book.author} description={book.description} />
+      })}
+
     </>
   )
 }
+
