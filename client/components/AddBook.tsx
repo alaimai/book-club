c92cfe81d48167ff73fe53d0be0d5ef7079f7d8d
Lei Zheng <zhenglei530@gmail.com>
2024-08-15 12:48:50 +1200

    api and hooks

---
+++ client/components/AddBook.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+  export function Home() {
+
+    return (
+      <>
+        <div>
+          <h1>Click the button to generate the dishes for a week</h1>
+        </div>
+        <div>
+          <Link to='/recipes'><button >🍲</button></Link>
+          
+        </div>
+      </>
+    )
+  }
+  
+}
