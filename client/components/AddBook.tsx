f32f1c497b12a7b10551fb31d77d4efe3e6ccdaa
Lei Zheng <zhenglei530@gmail.com>
2024-08-15 18:15:12 +1200

    refactor addbook component

--- client/components/AddBook.tsx
+++ client/components/AddBook.tsx
@@ -9,53 +9,58 @@ export function AddBook() {
     description: '',
     review: '',
   })
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     const { name, value } = e.target
     setBookInfo((prev) => ({ ...prev, [name]: value }))
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
-      if (
-        bookInfo.title === '' ||
-        bookInfo.description === '' ||
-        bookInfo.review === ''
-      ) {
-        return window.alert('* must not be empty')
-      }
+      // if (
+      //   bookInfo.title === '' ||
+      //   bookInfo.description === '' ||
+      //   bookInfo.review === ''
+      // ) {
+      //   return window.alert('* must not be empty')
+      // }
       addBook(bookInfo)
       setBookInfo({ title: '', description: '', review: '' })
     }
   }
+
   return (
     <>
+      {isError && <div>Failed to add book</div>}
+      {isSuccess && <div>New Book has been added</div>}
       <h2>Add New Book</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          * Title:
-          <input
-            type="text"
-            name="title"
-            value={bookInfo.title}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          * Description:
-          <input
-            type="text"
-            name="description"
-            value={bookInfo.description}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Review:
-          <input
-            type="text"
-            name="review"
-            value={bookInfo.review}
-            onChange={handleChange}
-          />
-        </label>
+        <label htmlFor="title">* Title:</label>
+        <input
+          required
+          id="title"
+          type="text"
+          name="title"
+          value={bookInfo.title}
+          onChange={handleChange}
+        />
+        <label htmlFor="description">* Description:</label>
+        <input
+          required
+          id="description"
+          type="text"
+          name="description"
+          value={bookInfo.description}
+          onChange={handleChange}
+        />
+        <label htmlFor="review">Review:</label>
+        <textarea
+          id="review"
+          name="review"
+          value={bookInfo.review}
+          onChange={handleChange}
+        />
+        <div></div>
+        <button>SUBMIT</button>
       </form>
     </>
   )
